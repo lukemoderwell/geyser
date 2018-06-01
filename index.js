7d09fdce33c9666b9feae96f66f436dab0f1a987
Luke Moderwell <luke.moderwell@gmail.com>
2018-06-01 12:25:43 -0400

    parceljs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('dist'));
 
 // Get YT Data
 app.get('/youtube', (req, res) => {
