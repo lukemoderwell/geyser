da54bc196daf5dfbe44e0627966d2573d9e6c15a
Luke Moderwell <luke.moderwell@gmail.com>
2018-06-01 15:42:39 -0400

    dynamic progress bar

--- public/client.js
+++ public/client.js
@@ -1,8 +1,10 @@
 import data from './data.json';
 
+const subGoal = 50000;
 const channels = data.channels;
 const subs = document.querySelectorAll('[data-metric="subscribers"]');
-const retention = document.querySelectorAll('[data-metric="retention"]');
+
+let totalSubs;
 
 function isPostive(n) {
   if (n > 0) {
@@ -11,19 +13,29 @@ function isPostive(n) {
   return n;
 }
 
-function fillProgress() {
+function countSubs() {
+  let count = 0;
+  for (var key in channels) {
+    count = count + channels[key].subscribers.amount;
+  }
+  totalSubs = count;
+  fillProgress(totalSubs);
+}
+
+function fillProgress(subs) {
+  const percent = (subs/subGoal) * 100;
   const bar = document.querySelector('[data-progress]');
-  const progress = bar.dataset.progress;
-  bar.style.width = `${progress}%`;
+  bar.dataset.progress = String(percent);
+  bar.style.width = `${percent}%`;
 }
 
 function init() {
+  countSubs();
   let subsChange = [];
   for (var key in channels) {
     if (channels.hasOwnProperty(key)) {
       subsChange.push(channels[key].subscribers.weeklyChange);
-    }
-    
+    } 
   }
   for (var i = 0; i < subs.length; i += 1) {
     var text = isPostive(subsChange[i]);
@@ -32,7 +44,6 @@ function init() {
     el.innerText = text;
     subs[i].appendChild(el);
   }
-
   fillProgress();
 }
 
