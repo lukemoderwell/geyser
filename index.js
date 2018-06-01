132884073ffbcbe898f13a8ed6a53a6132107b29
Luke Moderwell <luke.moderwell@gmail.com>
2018-06-01 09:15:43 -0400

    hello world

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+// Get YT Data
+app.get('/youtube', (req, res) => {
+  res.send('this is where we do the Youtubez');
+});
+
+app.listen(port, () => console.log(`Listening on ${port}`));
